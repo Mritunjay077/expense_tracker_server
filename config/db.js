a5b353691dca587d0d476ecf9e3cbe9851441158
Mritunjay077 <mritunjaykumarmukul@gmail.com>
2025-08-13 17:48:24 +0530

    add new env file

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
-// const mysql=require('mysql2');
+import dotenv from 'dotenv';
 import mysql from 'mysql2';
-const mysqlSetting={
-    host:'localhost',
-    user:'root',
-    password:'root',
-    port:3306,
-    database:'expense_tracker',
-    multipleStatements:true,
+
+// Load environment variables from .env (only in local dev)
+dotenv.config();
+
+const mysqlSetting = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
 };
 
-const conn=mysql.createPool(mysqlSetting);
+const conn = mysql.createPool(mysqlSetting);
 
 export default conn;
